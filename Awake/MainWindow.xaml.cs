b4422f9159fe01b7fdf3cf0380a910fc891b7b1e
Albert Akhmetov <akhmetov@live.com>
2025-03-09 20:58:36 +0500

    Prevent Screen Sleep in C# WinUI Apps (code sample)

---
+++ Awake/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Awake;
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Windows.Win32.System.Power;
+
+public partial class MainWindow : Window
+{
+    public MainWindow(IServiceProvider serviceProvider)
+    {
+        this.InitializeComponent();
+    }
+
+    private void toggle_Click(object sender, RoutedEventArgs e)
+    {
+        var state = EXECUTION_STATE.ES_CONTINUOUS;
+        if (toggle.IsChecked == true)
+        {
+            state |= EXECUTION_STATE.ES_DISPLAY_REQUIRED;
+        }
+
+        Windows.Win32.PInvoke.SetThreadExecutionState(state);
+    }
+}
